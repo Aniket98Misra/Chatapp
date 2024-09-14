820addbbbcf4362354e148be5590072372885321
Aniket98Misra <misra98aniket@gmail.com>
2024-09-14 19:35:51 +0530

    Backend completed except socketio, moving to ui

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../Utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -28,6 +29,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
+      //Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
@@ -44,9 +47,40 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: "Internal server Error" });
   }
 };
-export const login = (req, res) => {
-  console.log("login User");
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
 export const logout = (req, res) => {
-  console.log("logout User");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World??");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 const uri =
   "mongodb+srv://misra98aniket:JWfBzcqxNtdyLwds@cluster0.ujknh.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
@@ -31,4 +38,3 @@ app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on Port ${PORT}`);
 });
-//help me connect react to mongoose
